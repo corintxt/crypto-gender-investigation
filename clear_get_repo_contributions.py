6e21699f5862136abe2a7ec302d3649cbc8f5471
Corin Faife <corin.faife@gmail.com>
2019-03-20 18:21:40 -0400

    Add csv write method (not yet working correctly)

--- clear_get_repo_contributions.py
+++ clear_get_repo_contributions.py
@@ -1,6 +1,7 @@
 import json
 import requests
-import os
+import csv
+import config
 
 session = requests.session()
 ## Comment out Tor proxy for now.
@@ -8,7 +9,7 @@ session = requests.session()
 # session.proxies['http'] = 'socks5h://localhost:9050'
 # session.proxies['https'] = 'socks5h://localhost:9050'
 
-API_token = os.getenv("your_api_token_env_var")
+API_TOKEN = config.api_key
 
 api_url_base = 'https://api.github.com/'
 headers = {'Content-Type': 'application/json',
@@ -61,13 +62,8 @@ def build_contribution_list(contribution_response):
     return all_repo_contributions
 
 
-def lookup_human_name(profile_url):
-    # restate headers in case I need to change
-    lookup_headers = {'Content-Type': 'application/json',
-           'User-Agent': 'python-requests/3.6.1',
-           'Accept': 'application/vnd.github.v3+json'}
-    
-    response = session.get(profile_url, headers=lookup_headers)
+def lookup_human_name(profile_url):    
+    response = session.get(profile_url, headers=headers)
 
     if response.status_code == 200:
         return (response.json()['name'])
@@ -75,21 +71,44 @@ def lookup_human_name(profile_url):
         print('[!] HTTP {0} looking up user [{1}]'.format(response.status_code, profile_url))
         return None
 
+
+def append_list_to_csv(mylist, output_file):
+    with open(output_file, 'a') as myfile:
+        wr = csv.writer(myfile, quoting=csv.QUOTE_ALL)
+        wr.writerow(mylist)
+
 # define list variable outside of get_all_contributions function
 all_org_contributions = list()
   
 def get_all_contributions(org):
+    print("Retrieving list of all repos for {}".format(org))
     repos = get_repos(org)
 
     for repo in repos:
+        print("Building contributor commit list for {}".format(repo['full_name']))
         contributors = get_contributors(repo)
         contribution_list = build_contribution_list(contributors)
         all_org_contributions.append(contribution_list)
 
     # new API call to add real names to dictionary
+    print("Matching real names against contributor usernames...")
     for i in range (0, len(all_org_contributions)):
+        print("Searching repo {} of {}".format(i,len(all_org_contributions)))
         for j in range (0, len(all_org_contributions[i])):
             human_name = lookup_human_name(all_org_contributions[i][j]['profile_url'])
             all_org_contributions[i][j]['name'] = human_name
 
-    print("Contribution list complete.")
+    print("Contribution list complete!")
+
+############
+
+## This part isn't in the right format
+    print("Writing to file.")
+
+    file_name = "{}.csv".format(org)
+    flat_contributions = [item for sublist in all_org_contributions for item in sublist]
+    ## Do this row by row.
+    for item in flat_contributions:
+        append_list_to_csv(item, file_name)
+
+    print("Contributor list saved as {}".format(file_name))
