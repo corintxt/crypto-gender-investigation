0b1e9edf05b91a35bf7703b393331e3fe1555fa8
Corin Faife <corin.faife@gmail.com>
2019-04-02 15:24:22 -0400

    Write script to make gender queries from input csv

---
+++ notes.md
@@ -0,0 +1,5 @@
+Ultimately I want to assemble a CSV with a list of company names & GitHub org names, and tell the script to slowly run through it, getting all of the commit data, then parsing for gender.
+
+Step 1: Assemble data for all companies
+
+Step 2: Give gender scraper a directory to look in where CSVs are stored
--- parse_gender.py
+++ parse_gender.py
@@ -1,5 +1,6 @@
 import requests
 import json
+import pandas as pd
 
 
 api_url_base = 'https://api.genderize.io/'
@@ -7,9 +8,36 @@ api_url_base = 'https://api.genderize.io/'
 def get_gender(firstname):
     request_url = '{}?name={}'.format(api_url_base, firstname)
     response = requests.get(request_url)
+    print("Genderizing name: {}".format(firstname))
     
     if response.status_code == 200:
         return (response.json())
     else:
         print('[!] HTTP {0} looking up name [{1}]'.format(response.status_code, firstname))
         return None
+
+
+def lookup_genders(csv, output):
+    df = pd.read_csv(csv)
+    df.columns = ['repo', 'username', 'contributions', 
+                'avatar_url', 'profile_url', 'real_name']
+
+    gender_list = list()
+
+    for name in df['real_name'].unique():
+        if type(name) == str: # don't call on NaN values (which are float)
+            first_name = name.split(' ')[0]
+            gender_result = get_gender(first_name)
+            gender_result['real_name'] = name # add real name back to dictionary
+            gender_list.append(gender_result)      
+
+    # Convert to dataframe and merge with data from original csv
+    gender_df = pd.DataFrame(gender_list)
+    full_frame = pd.merge(df, gender_df[['real_name', 'gender', 'probability']], 
+               on='real_name', how='outer')
+
+    full_frame.to_csv(output, index=False, header=False)
+
+if __name__ == '__main__':
+    target_csv = str(input('Enter csv file to genderize: > '))
+    lookup_genders(target_csv, '/gendered/{}'.format(target_csv))
